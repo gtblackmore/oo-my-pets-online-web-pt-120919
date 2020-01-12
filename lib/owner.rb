28960f29ba46f63963cc3224fe0af15db7e66d23
George Blackmore <gtblackmore@gmail.com>
2020-01-12 20:25:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,18 +1,32 @@
 class Owner
   attr_accessor :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name
     @species = "Human"
+    @@all << name
   end
   
   def name=(name)
-    
+    @name
+  end
+  
+  def species
+    @species = "Human"
+  end
+  
+  def say_species
+    puts species
+  end
   
   def self.all
+    @@all
   end
   
   def self.count
+    @@all.
   end
   
   def self.reset_all
